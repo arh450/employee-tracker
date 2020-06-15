1e2d54f8e0637ca66553bf411530831eafffbacd
hank amadeus <hankamadeus@email.com>
2020-06-15 14:51:54 -0400

    added some pseudo code to create basic layout of map and order of todos

--- server.js
+++ server.js
@@ -19,3 +19,35 @@ connection.connect((err) => {
     console.log(`connected as id ${connection.threadId}`);
 
 });
+
+
+// MINIMUM REQUIREMENTS
+    //  * Add departments, roles, employees
+    //  * View departments, roles, employees
+    //  * Update employee roles
+
+// BONUS FEATURES
+    // * Update employee managers
+    // * View employees by manager
+    // * Delete departments, roles, and employees
+    // * View the total utilized budget of a department -- ie the combined salaries of all employees in that department
+
+// NPM PACKAGES
+    // * inquirer (installed)
+    // * mysql (installed)
+    // * dotenv (installed)
+    // * console.table (installed)
+
+// SCHEMA
+    // * DEPARTMENT TABLE (DONE)
+    // * ROLE (DONE)
+    // * EMPLOYEE (DONE)
+
+//  SEEDS
+    //  PRE-POPULATE DB
+
+//  OTHER THINGS TO CONSIDER
+    // CLASS FOR SQL QUERIES
+    // USE SQL JOIN
+    // CLASS FOR INQUIRER QUESTIONS(?)
+
