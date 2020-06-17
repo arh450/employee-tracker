ed1affc78ee85cdd86ff8eadacea7a5f274c5890
hank amadeus <hankamadeus@email.com>
2020-06-17 14:46:38 -0400

    tried to make class for inquirer functions, to no avail

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const connection = require("./lib/connection");
+const { mOptions, aDeptInput, aRoleInput, aEmpInput } = require("./lib/prompts")
 const { prompt } = require("inquirer");
-const { mOptions, aDeptInput, aRoleInput, aEmpInput } = require("./lib/prompts");
+
 
 
 const selectMenu = () => {
@@ -107,4 +108,38 @@ const quit = () => {
     connection.end();
 };
 
+
 selectMenu();
+
+
+// Need Select menu prompt to trigger what task a user is trying to do
+
+// MINIMUM REQUIREMENTS
+//  * Add departments, roles, employees
+//  * View departments, roles, employees
+//  * Update employee roles
+
+// BONUS FEATURES
+// * Update employee managers
+// * View employees by manager
+// * Delete departments, roles, and employees
+// * View the total utilized budget of a department -- ie the combined salaries of all employees in that department
+
+// NPM PACKAGES
+// * inquirer (installed)
+// * mysql (installed)
+// * dotenv (installed)
+// * console.table (installed)
+
+// SCHEMA
+// * DEPARTMENT TABLE (DONE)
+// * ROLE (DONE)
+// * EMPLOYEE (DONE)
+
+//  SEEDS
+//  PRE-POPULATE DB (Done)
+
+//  OTHER THINGS TO CONSIDER
+// CLASS FOR SQL QUERIES
+// USE SQL JOIN
+// CLASS FOR INQUIRER QUESTIONS(?)
