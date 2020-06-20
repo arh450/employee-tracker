fcda364d44e14c13f70311c1ffd196649b610eb5
hank amadeus <hankamadeus@email.com>
2020-06-19 21:02:39 -0400

    starting new query class to good results, added three add functions...WIP

--- app.js
+++ app.js
@@ -1,109 +1,56 @@
+const Queries = require("./lib/Queries");
 
 const { prompt } = require("inquirer");
+const { mOptions } = require('./lib/prompts');
 
 const selectMenu = () => {
-    prompt(prompts.mOptions).then(({ selection }) => {
+    prompt(mOptions).then(({ selection }) => {
         if (selection === "Add Department") {
-            addDepartment();
+            Queries.addDepartment();
         } else if (selection === "Add Role") {
-            addRole();
+            Queries.addRole();
         } else if (selection === "Add Employee") {
-            addEmployee();
+            Queries.addEmployee();
         } else if (selection === "View Departments") {
-            viewDepartments();
+            Queries.viewDepartments();
         } else if (selection === "View Roles") {
-            viewRoles();
+            Queries.viewRoles();
         } else if (selection === "View Employees") {
-            viewEmployees();
+            Queries.viewEmployees();
         } else {
-            quit();
+            Queries.quit();
         }
     });
 };
 
-const addDepartment = () => {
-    prompt(prompts.aDeptInput).then(({ dName }) => {
-        connection.query(
-            "INSERT INTO department SET ?",
-            {
-                name: dName,
-            },
-            (err) => {
-                if (err) {
-                    throw err;
-                }
-                console.log("Department Added");
-                selectMenu();
-            }
-        );
-    });
-};
 
-const addRole = () => {
-    prompt(prompts.aRoleInput).then(({ rTitle, rSalary, rDepartmentID }) => {
-        connection.query(
-            "INSERT INTO role SET ?",
-            {
-                title: rTitle,
-                salary: rSalary,
-                department_id: rDepartmentID,
-            },
-            (err) => {
-                if (err) {
-                    throw err;
-                }
-                console.log("Role Added");
-                selectMenu();
-            }
-        );
-    });
-};
 
-const addEmployee = () => {
-    prompt(prompts.aEmpInput).then(({ eFirstName, eLastName, eRoleID, eManagerID }) => {
-        connection.query(
-            "INSERT INTO employee SET ?",
-            {
-                first_name: eFirstName,
-                last_name: eLastName,
-                role_id: eRoleID,
-                manager_id: eManagerID,
-            },
-            (err) => {
-                if (err) {
-                    throw err;
-                }
-                console.log("Employee Added");
-                selectMenu();
-            }
-        );
-    });
-};
 
-const viewDepartments = () => {
-    connection.query("SELECT * FROM employee_tracker.department", (err, res) => {
-        console.table(res);
-        selectMenu();
-    });
-};
 
-const viewRoles = () => {
-    connection.query("SELECT * FROM employee_tracker.role", (err, res) => {
-        console.table(res);
-        selectMenu();
-    });
-};
+// const viewDepartments = () => {
+//     connection.query("SELECT * FROM employee_tracker.department", (err, res) => {
+//         console.table(res);
+//         selectMenu();
+//     });
+// };
 
-const viewEmployees = () => {
-    connection.query("SELECT * FROM employee_tracker.employee", (err, res) => {
-        console.table(res);
-        selectMenu();
-    });
-};
+// const viewRoles = () => {
+//     connection.query("SELECT * FROM employee_tracker.role", (err, res) => {
+//         console.table(res);
+//         selectMenu();
+//     });
+// };
 
-const quit = () => {
-    connection.end();
-};
+// const viewEmployees = () => {
+//     connection.query("SELECT * FROM employee_tracker.employee", (err, res) => {
+//         console.table(res);
+//         selectMenu();
+//     });
+// };
+
+// const quit = () => {
+//     connection.end();
+// };
 
 
 selectMenu();
